1c34062e8ebb07c7437efeae6fba0b6d84ebb42e
Rhian Davies <thetrianglegirl@gmail.com>
2025-08-18 14:18:35 +0100

    🗃️ Update data and replace freeze with site

--- R/set_up.R
+++ R/set_up.R
@@ -2,39 +2,54 @@
 
 source("R/functions.R")
 
-# Simple meta data
-all_paddles <- get_paddles()
-sf::st_write(all_paddles, "data/paddles.geojson", append = FALSE)
-
-# Compare against RDS
-detailed_paddles <- readRDS("data/paddles.rds")
-missing_activities <- all_paddles$id[which(!(all_paddles$id %in% detailed_paddles$id))]
-activities_to_import <- missing_activities[1:10]
-
-new_detailed <-
-  all_paddles |>
-  dplyr::filter(id %in% activities_to_import) |>
-  dplyr::mutate(detailed = purrr::map(id, \(x) rStrava::get_activity(id = x, strava_token)))
-
-updated_detailed <- rbind(detailed_paddles, new_detailed)
-
-saveRDS(updated_detailed, "data/paddles.rds")
+get_raw()
+get_paddles()
+get_latest()
 
 # update_activity(12021786501)
 
-# Create nice format
 detailed_activities <-
   readRDS("data/paddles.rds") |>
   dplyr::mutate(
+    name = sanitise_name(name),
     slug = slugify(name),
-    date = stringr::str_sub(start_date, 1, 10),
+    start_date = purrr::map_chr(detailed, \(x) {
+      purrr::pluck(x, "start_date") |>
+        lubridate::ymd_hms() |>
+        format("%Y-%m-%d")
+    }),
     categories = purrr::map(detailed, get_categories),
     strava_url = glue::glue("https://www.strava.com/activities/{id}"),
-    image_url = purrr::map_chr(detailed, \(x) purrr::pluck(x, "photos", "primary", "urls", "600", .default = NA_character_)),
-    description = purrr::map_chr(detailed, \(x) purrr::pluck(x, "description")),
-    filename = glue::glue("posts/{date}_{slug}.qmd")
-  ) |>
-  dplyr::filter(!is.na(image_url)) # TODO
+    image_url = purrr::map_chr(detailed, \(x) {
+      purrr::pluck(
+        x,
+        "photos",
+        "primary",
+        "urls",
+        "600",
+        .default = "https://placehold.co/600x400/EEE/31343C"
+      )
+    }),
+    description = purrr::map_chr(detailed, \(x) {
+      purrr::pluck(x, "description", .default = "TO DO")
+    }),
+    filename = glue::glue("posts/{start_date}_{slug}.qmd")
+  )
 
-# Generate all the post files from detailed activites
+# Generate all the post files from detailed activities
+source("R/paddle_post.R")
 purrr::walk(detailed_activities$id, \(x) paddle_post(x, detailed_activities))
+
+
+# FIX MALFORMED DATES (still an issue)
+
+fix <- readRDS("data/paddles.rds") |>
+  dplyr::mutate(
+    start_date = purrr::map_chr(detailed, \(x) {
+      purrr::pluck(x, "start_date") |>
+        lubridate::ymd_hms() |>
+        format("%Y-%m-%d")
+    })
+  )
+
+#saveRDS(fix, "data/paddles.rds")
